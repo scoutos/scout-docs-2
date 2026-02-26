b683ed4c6a396812244bab3ec7c50909760ceca5
twilson63 <twilson63@hey.com>
2026-02-26 14:30:58 -0500

    fix: correct docsRepositoryBase to use correct repo name (scout-docs-2)
    - Updated docsRepositoryBase from 'scout-docs2' to 'scout-docs-2'
    - This fixes 'Edit this page' links to point to correct GitHub repo
    - Navbar projectLink was already correct

--- app/layout.jsx
+++ app/layout.jsx
@@ -43,7 +43,7 @@ export default async function RootLayout({ children }) {
           banner={banner}
           navbar={navbar}
           pageMap={await getPageMap()}
-          docsRepositoryBase="https://github.com/scoutos/scout-docs2"
+          docsRepositoryBase="https://github.com/scoutos/scout-docs-2"
           footer={footer}
         >
           {children}
