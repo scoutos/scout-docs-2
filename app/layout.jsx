ffb330859118a05fab89dff5d0891627ab2690d8
Tom Wilson <tom@hyper.io>
2026-02-27 14:18:06 -0500

    docs: fix edit links and enable static search indexing

--- app/layout.jsx
+++ app/layout.jsx
@@ -43,7 +43,7 @@ export default async function RootLayout({ children }) {
           banner={banner}
           navbar={navbar}
           pageMap={await getPageMap()}
-          docsRepositoryBase="https://github.com/scoutos/scout-docs-2"
+          docsRepositoryBase="https://github.com/scoutos/scout-docs-2/blob/main"
           footer={footer}
         >
           {children}
